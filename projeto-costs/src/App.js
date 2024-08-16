6345fdb770c28a19f55f6360ba4d4d58a48bd39d
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-08-16 14:14:24 -0300

    Entendendo o JSX

--- projeto-costs/src/App.js
+++ projeto-costs/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Lorenzo'
+
   return (
     <div className="App">
       <h1>Hello React World</h1>
       <p>Pronto pra codar o Costs?</p>
+      <p>Vamos nessa, {name.toUpperCase()}?</p>
     </div>
   );
 }
