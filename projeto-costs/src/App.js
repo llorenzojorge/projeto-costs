32f5b2f3a9b19bef9aadce9bdf2fc777f10a90a3
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-08-16 14:40:29 -0300

    create Frase.js

--- projeto-costs/src/App.js
+++ projeto-costs/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   const name = 'Lorenzo'
@@ -8,6 +9,7 @@ function App() {
       <h1>Hello React World</h1>
       <p>Pronto pra codar o Costs?</p>
       <p>Vamos nessa, {name.toUpperCase()}?</p>
+      <HelloWorld />
     </div>
   );
 }
---
+++ projeto-costs/src/components/Frase.js
@@ -0,0 +1,9 @@
+function Frase() {
+    return (
+        <div>
+            <p>Esse é um componente novo chamado 'Frase.js'</p>
+        </div>
+    )
+}
+
+export default Frase
