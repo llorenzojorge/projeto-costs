11a5f668619e0f76d4cda9be720e996af6115699
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-08-19 17:41:38 -0300

    add diversos props

--- projeto-costs/src/components/Pessoa.js
+++ projeto-costs/src/components/Pessoa.js
@@ -1,6 +1,11 @@
-function Pessoa() {
+function Pessoa(props) {
     return (
-
+        <div>
+            <img src={props.foto} alt={props.nome}/>
+            <p>Nome: {props.nome}</p>
+            <p>Idade: {props.idade}</p>
+            <p>Profissão: {props.profissao}</p>
+        </div>
     )
 }
 
