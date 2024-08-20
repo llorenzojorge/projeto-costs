04ad6f77887317755a31fd7739d50795f83f7832
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-08-20 00:02:12 -0300

    add Lista

--- projeto-costs/src/App.js
+++ projeto-costs/src/App.js
@@ -1,20 +1,17 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import List from './components/List';
 import Pessoa from './components/Pessoa';
 import SayMyName from './components/SayMyName';
 
 function App() {
-  const name = 'Lorenzo'
-
   return (
     <div className="App">
-      <h1>Hello React World</h1>
-      <p>Pronto pra codar o Costs?</p>
-      <p>Vamos nessa, {name.toUpperCase()}?</p>
       <HelloWorld />
       <SayMyName nome="Maria"/>
       <SayMyName nome="Leandro"/>
       <Pessoa foto="https://via.placeholder.com/150" nome="Lorenzo" idade="19" profissao="Web Developer"/>
+      <List />
     </div>
   );
 }
--- projeto-costs/src/components/List.js
+++ projeto-costs/src/components/List.js
@@ -1,7 +1,12 @@
 function List() {
     return (
         <>
-        <h1>Minha List</h1>
+        <h1>Minha Lista</h1>
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+        </ul>
         </>
     )
 }
