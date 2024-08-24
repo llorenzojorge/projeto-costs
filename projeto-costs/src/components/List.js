237f2a67933e9a89e56b6a22e52838ea73c4b629
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-08-24 12:28:40 -0300

    diferença entre "" e {}

--- projeto-costs/src/components/List.js
+++ projeto-costs/src/components/List.js
@@ -1,11 +1,13 @@
+import Item from "./Item"
+
 function List() {
     return (
         <>
         <h1>Minha Lista</h1>
         <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
+            <Item marca="Honda" ano_lancamento={1980}/>
+            <Item marca="Yamaha" ano_lancamento={1978}/>
+            <Item marca="Kawasaki" ano_lancamento={1986}/>
         </ul>
         </>
     )
