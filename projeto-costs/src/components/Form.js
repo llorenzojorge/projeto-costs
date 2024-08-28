21fff97f1488211708458f301ab6a68cd6d289e5
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-08-28 17:31:52 -0300

    Criei um form

--- projeto-costs/src/components/Form.js
+++ projeto-costs/src/components/Form.js
@@ -2,6 +2,14 @@ function Form() {
     return (
         <div>
             <h1>Meu cadastro</h1>
+            <form>
+                <div>
+                    <input type="text" placeholder="Digite seu nome"/>
+                </div>
+                <div>
+                    <input type="submit" value="Cadastrar"/>
+                </div>
+            </form>
         </div>
     )
 }
