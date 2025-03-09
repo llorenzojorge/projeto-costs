76c3ac5b5886b915299493edf2b179eb90cc2fe6
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2025-03-09 15:38:43 -0300

    otimização

--- projeto-costs/src/App.js
+++ projeto-costs/src/App.js
@@ -3,15 +3,6 @@ import HelloWorld from './components/HelloWorld';
 
 function App() {
 
-  const name = 'Lorenzo'
-  const newName = name.toUpperCase()
-
-  const url = 'https://placehold.co/150'
-
-  function soma(a, b) {
-    return a + b
-  }
-
   return (
     <div className="App">
       <HelloWorld />
