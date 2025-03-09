19dce2daa1ff382186ad84e1a41fdb028a5c9885
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2025-03-09 14:25:13 -0300

    Aplicando váriaveis no JSX

--- projeto-costs/src/App.js
+++ projeto-costs/src/App.js
@@ -1,14 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
   const name = 'Lorenzo'
+  const newName = name.toUpperCase()
+
+  const url = 'https://placehold.co/150'
+
+  function soma(a, b) {
+    return a + b
+  }
 
   return (
     <div className="App">
-     <h1>Entendendo o JSX</h1>
-     <p>Olá, {name}</p>
+      <h1>Entendendo o JSX</h1>
+      <p>Olá, {name}</p>
+      <p>Soma: {soma(5, 10)}</p>
+      <img src={url} alt="Imagem de 150px"/>
     </div>
   );
 }
