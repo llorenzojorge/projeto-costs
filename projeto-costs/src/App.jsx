484d19a446e99e59778f314a31697dbe36aadd7e
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2025-03-16 16:01:22 -0300

    Inserindo CSS no react

--- projeto-costs/src/App.jsx
+++ projeto-costs/src/App.jsx
@@ -2,18 +2,15 @@ import './App.css';
 import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
 
-  const nome = "Angolan"
-
   return (
     <div className="App">
-
-      <SayMyName nome="Lorenzo" />
-      <SayMyName nome="Caio" />
-      <SayMyName nome={nome} />
-
+      <Frase />
+      <Frase />
+      
       <Pessoa nome="Lorenzo" idade="20" profissao="Progamador" foto="https://placehold.co/250" />
 
     </div>
--- projeto-costs/src/components/Frase.jsx
+++ projeto-costs/src/components/Frase.jsx
@@ -1,7 +1,11 @@
+import styles from './Frase.module.css'
+
 function Frase() {
 
     return (
-        <p>Esse é um componente com um frase.</p>
+        <div className={styles.fraseContainer}>
+            <p className={styles.fraseContent}>Esse é um componente com um frase.</p>
+        </div>
     )
 }
 
